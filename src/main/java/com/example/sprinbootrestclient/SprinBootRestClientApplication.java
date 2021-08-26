19f61ef24254147d53bbdfd75c7795b4aa5e6e62
BeobSik <tjswl756@naver.com>
2021-08-26 13:04:34 +0900

    init

---
+++ src/main/java/com/example/sprinbootrestclient/SprinBootRestClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.sprinbootrestclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinBootRestClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprinBootRestClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sprinbootrestclient/SprinBootRestClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sprinbootrestclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprinBootRestClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
