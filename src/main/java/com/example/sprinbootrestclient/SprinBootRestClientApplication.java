cd0605c97c1a5014b65f8d7048f4d35a557dac5e
BeobSik <tjswl756@naver.com>
2021-08-26 13:20:57 +0900

    feat: global customizing

--- src/main/java/com/example/sprinbootrestclient/SprinBootRestClientApplication.java
+++ src/main/java/com/example/sprinbootrestclient/SprinBootRestClientApplication.java
@@ -2,12 +2,20 @@ package com.example.sprinbootrestclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.reactive.function.client.WebClientCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class SprinBootRestClientApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SprinBootRestClientApplication.class, args);
+    }
+
+    @Bean
+    public WebClientCustomizer webClientCustomizer(){
+        return webClientBuilder -> webClientBuilder.baseUrl("http://localhost:8080");
     }
 
 }
